4c17bb0eb6d8ff2f02bc2b3da6ec91c8842e09eb
Ana Martins <aimartin@uio.no>
2024-08-08 15:52:02 +0200

    standard input/output

--- variables.f90
+++ variables.f90
@@ -13,4 +13,10 @@ program variables
     initial = 'A'
     isOkay = .false.
 
+    print *, 'The value of amount (integer) is: ', amount
+    print *, 'The value of pi (real) is: ', pi
+    print *, 'The value of frequency (complex) is: ', frequency
+    print *, 'The value of initial (character) is: ', initial
+    print *, 'The value of isOkay (logical) is: ', isOkay
+
 end program variables
